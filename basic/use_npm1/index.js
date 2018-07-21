5a6b76868d5a3bce923bf95e02d175ecbd412210
gbox3d <gbox3d@gmail.com>
2018-07-21 13:05:25 +0900

    npm usage sample

---
+++ basic/use_npm1/index.js
@@ -0,0 +1,13 @@
+console.log('hello');
+
+
+var async = require('async');
+var moment = require('moment');
+
+//import _async from 'async'
+
+console.log(async);
+console.log(moment);
+
+
+//console.log(_async);
